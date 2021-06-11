7026b17e8e0132928e75e399140d523bfd6297d5
Kishor <kishorarya.che18@itbhu.ac.in>
2021-06-12 01:51:13 +0530

    add

---
+++ src/db/connect.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/kissu',{useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true});
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error;'))
+db.once('open',function(){
+    console.log("we are connected..")
+});
+
+const Details = new mongoose.Schema({
+    ownername:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    kitchenname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    Fssaino:{
+        type:Number,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    preptime:{
+        type:String,
+        required:true
+    },
+    food:{
+        type:String,
+        required:true
+    },
+    breakfastfrom:{
+        type:String,
+        required:true
+    },
+    lunchfrom:{
+        type:String,
+        required:true
+    },
+    highteafrom:{
+        type:String,
+        required:true
+    },
+    dinnerfrom:{
+        type:String,
+        required:true
+    },
+    breakfastto:{
+        type:String,
+        required:true
+    },
+    lunchto:{
+        type:String,
+        required:true
+    },
+    highteagto:{
+        type:String,
+        required:true
+    },
+    dinnerto:{
+        type:String,
+        required:true
+    },
+});
+
+const Register = mongoose.model('Register',Details)
+
+module.exports = Register;
